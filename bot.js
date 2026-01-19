1e10fa05fdcc4a5a769cd5bc4a98f67e21108315
Atlas <164876872+RenathoM@users.noreply.github.com>
2026-01-19 15:06:54 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5330,7 +5330,7 @@ async function getRobloxAvatarUrl(userId) {
 
       // For now, show instruction
       await interaction.reply({
-        content: '📸 Please attach the proof image to your next message in this channel.\n\n| > Take a screenshot of the screen where you are making the trade, send the image via any DM, go to the image page, copy the image link, and paste the image link into the input box of the "Upload Proof Image" button.',
+        content: '📸 Please attach the proof image to your next message in this channel.\n\n**Take a screenshot of the screen where you are making the trade, send the image via any DM, go to the image page, copy the image link, and paste the image link into the input box of the "Upload Proof Image" button.**',
         ephemeral: false
       });
 
@@ -5347,7 +5347,7 @@ async function getRobloxAvatarUrl(userId) {
 
       // Show instruction
       await interaction.reply({
-        content: '📸 Please attach the proof image to your next message in this channel.\n\n| > Take a screenshot of the screen where you are making the trade, send the image via any DM, go to the image page, copy the image link, and paste the image link into the input box of the "Upload Proof Image" button.',
+        content: '📸 Please attach the proof image to your next message in this channel.\n\n**Take a screenshot of the screen where you are making the trade, send the image via any DM, go to the image page, copy the image link, and paste the image link into the input box of the "Upload Proof Image" button.**',
         ephemeral: false
       });
 
